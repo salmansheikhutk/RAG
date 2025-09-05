93cde35b64ff424e55ef72816b1a4148406107f3
salmansheikhutk <32319474+salmansheikhutk@users.noreply.github.com>
2025-09-05 10:17:51 -0400

    Add IAM policy configuration for ticket_004

---
+++ terraform/iam-policies/ticket_004/main.tf
@@ -0,0 +1,50 @@
+
+# IAM Policy Creation for DataScienceRole
+# ServiceNow Ticket: Create new policy for epro-* access
+# Action: Creating new policy (no existing policy found)
+
+# Reference existing IAM role
+data "aws_iam_role" "existing_role" {
+  name = "DataScienceRole"
+}
+
+# Create new S3 access policy
+resource "aws_iam_policy" "s3_read_policy" {
+  name        = "S3ReadAccess-epro-All-Policy"
+  description = "Read access to epro-* S3 buckets"
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::epro-*",
+          "arn:aws:s3:::epro-*/*"
+        ]
+      }
+    ]
+  })
+}
+
+# Attach new policy to existing role
+resource "aws_iam_role_policy_attachment" "attach_s3_read_policy" {
+  role       = data.aws_iam_role.existing_role.name
+  policy_arn = aws_iam_policy.s3_read_policy.arn
+}
+
+# Output new policy information
+output "new_policy_arn" {
+  description = "ARN of the created S3 read access policy"
+  value       = aws_iam_policy.s3_read_policy.arn
+}
+
+output "attachment_status" {
+  description = "Status of policy attachment to role"
+  value       = "New policy attached to ${data.aws_iam_role.existing_role.name}"
+}
